e22178419115c92bc433b69150b93096a6d30fa7
UrosTim <urostimapps@gmail.com>
2024-07-10 11:15:40 +0200

    Initial commit

--- src/app/user/create/page.js
+++ src/app/user/create/page.js
@@ -1,45 +1,99 @@
 'use client';
 
-import Link from "next/link";
-import {useEffect, useState} from "react";
-import {get} from "@/core/httpClient";
+import {useForm} from "react-hook-form";
+import {Button, Col, Row} from "reactstrap";
+import {post} from "@/core/httpClient";
 
 export default function UserCreate() {
-    const [counter, setCounter] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState();
 
-    const getFirstName = async () => {
-        setLoading(true);
-
-        let result = await get("/user/get-first-name");
-
-        setData(result.data);
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        getFirstName();
-    }, [counter]);
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({mode: "onSubmit"});
 
     return (
         <>
-            {loading === true ? <h1>Loading...</h1> : (
-                <>
-                    <h1>{data}</h1>
-                    {/*<h1>{counter}</h1>*/}
-                    {/*<br/>*/}
-                    {/*<br/>*/}
-                    <button onClick={() => {
-                        setCounter(counter + 1)
-                    }}>Add One
-                    </button>
-                    {/*<h1>User create page.</h1>*/}
-                    {/*<br/>*/}
-                    {/*<br/>*/}
-                    {/*<Link href="/user/list">Go to list page.</Link>*/}
-                </>
-            )}
+            <Row className="mb-4">
+                <Col md={6}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="First Name"
+                        {...register("firstName", {
+                            required: "First Name is required.",
+                            maxLength: 50,
+                            minLength: 2,
+                        })} />
+                    {errors && errors.firstName && (
+                        <span className="text-danger">
+                            {errors.firstName.message}
+                        </span>
+                    )}
+                </Col>
+                <Col md={6}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Last Name"
+                        {...register("lastName", {
+                            required: "Last Name is required.",
+                            maxLength: 50,
+                            minLength: 2,
+                        })} />
+                    {errors && errors.lastName && (
+                        <span className="text-danger">
+                            {errors.lastName.message}
+                        </span>
+                    )}
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col md={6}>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        {...register("email", {
+                            required: "Email is required.",
+                        })} />
+                    {errors && errors.email && (
+                        <span className="text-danger">
+                            {errors.email.message}
+                        </span>
+                    )}
+                </Col>
+                <Col md={6}>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="Contact Number"
+                           {...register("contactNumber", {
+                               required: "Contact Number is required.",
+                               validate: (value) => {
+                                   if (!/^[0-9]*$/.test(value)) {
+                                       return "Please enter a valid number";
+                                   }
+                               }
+                           })} />
+                    {errors && errors.contactNumber && (
+                        <span className="text-danger">
+                            {errors.contactNumber.message}
+                        </span>
+                    )}
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-end">
+                    <Button className="btn btn-primary" type="button" onClick={() => {
+                        handleSubmit(async (data) => {
+                            await post("/user/create-user-body", data);
+                        })();
+                    }}>
+                        Submit
+                    </Button>
+                </Col>
+            </Row>
         </>
     )
 }
